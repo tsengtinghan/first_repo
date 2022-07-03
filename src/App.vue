1a99e168d1d501ec23e4262e64f3f39db1887c4f
Michael Tseng <michael08120@gmail.com>
2022-07-03 10:51:20 +0800

    first commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import NAV from './components/NAV.vue'
+</script>
+
+<template>
+  <NAV/>
+</template>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #1b1457;
+  margin-top: 0px;
+}
+</style>
